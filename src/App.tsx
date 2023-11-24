b344c4a1c296c6c3b8832bfcbc91318952d48d8e
nicenicegame <nicenicegame@gmail.com>
2023-11-25 00:06:55 +0700

    add files

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from 'react'
+
+function App() {
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const [screen, setScreen] = useState<{ screenX: number; screenY: number }>({
+    screenX: 0,
+    screenY: 0
+  })
+
+  useEffect(() => {
+    const fetchUserMedia = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+      })
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream
+        videoRef.current.width = window.screen.availWidth
+        videoRef.current.height = window.screen.availHeight
+        videoRef.current.play()
+      }
+    }
+    fetchUserMedia()
+  }, [])
+
+  useEffect(() => {
+    const update = setInterval(() => {
+      setScreen({ screenX: window.screenX, screenY: window.screenY })
+    }, 1000)
+
+    return () => clearInterval(update)
+  }, [])
+
+  return (
+    <video
+      ref={videoRef}
+      style={{
+        transform: `translate(-${screen.screenX}px, -${screen.screenY}px)`
+      }}
+    />
+  )
+}
+
+export default App
